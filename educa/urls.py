267ff4da8f719bcbe160df82f44c7013e2017c46
SRICKKY <kumar_srikant@outlook.com>
2019-08-08 17:01:58 +0530

    MEDIA STATIC PATH setting for Debug Mode

--- educa/urls.py
+++ educa/urls.py
@@ -3,6 +3,8 @@
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 from courses.views import CourseListView
@@ -17,3 +19,7 @@ urlpatterns = [
          name='course_list'),
     path('students/', include('students.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
